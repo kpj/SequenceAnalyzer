c069e4f612170efd03183293aa7c9bcd85cd07f0
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-10-03 10:20:39 +0200

    Editable settings

--- data/settings.js
+++ data/settings.js
@@ -1,2 +1,19 @@
 // gap penalty
 gap_penalty = 8;
+
+// retrieves new values/sets default ones if necessary
+function update_settings() {
+	$("#options").children("input").each(function(i) {
+		var id = $(this)[0].id;
+
+		// update variables
+		var js_value = eval($(this)[0].id);
+		var html_value = $(this).val();
+
+		if(html_value == "") {
+			$(this).val(js_value);
+		} else {
+			window[id] = html_value;
+		}
+	});
+}
