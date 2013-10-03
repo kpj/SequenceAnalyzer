474ba5ece61875699c1f2354eda7d9d11e0b45a3
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-10-03 10:32:43 +0200

    Added PAM250 and matrix selector

--- data/settings.js
+++ data/settings.js
@@ -1,19 +1,27 @@
 // gap penalty
 gap_penalty = 8;
 
+// substitution matrix to use
+subst_matrix_name = ""
+
+
 // retrieves new values/sets default ones if necessary
 function update_settings() {
-	$("#options").children("input").each(function(i) {
+	$("#options").children().each(function(i) {
 		var id = $(this)[0].id;
 
 		// update variables
-		var js_value = eval($(this)[0].id);
-		var html_value = $(this).val();
+		if($(this).attr("type") == "text") {
+			var js_value = eval($(this)[0].id);
+			var html_value = $(this).val();
 
-		if(html_value == "") {
-			$(this).val(js_value);
-		} else {
-			window[id] = html_value;
+			if(html_value == "") {
+				$(this).val(js_value);
+			} else {
+				window[id] = html_value;
+			}
+		} else if($(this).attr("type") == "dropdown") {
+			subst_matrix_name = $(this).val();
 		}
 	});
 }
